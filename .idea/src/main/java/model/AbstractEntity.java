fdfb7f755f860f03f76cf095540cf6615552807f
Carolina Varzari <varzari3009@gmail.com>
2026-05-07 14:39:27 +0300

    Creare super clasa

---
+++ .idea/src/main/java/model/AbstractEntity.java
@@ -0,0 +1,30 @@
+package model;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Version
+    protected Integer version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
